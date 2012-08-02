793087a6b8f4deea7f69b6782b141df501927f1f
Matt Hamilton <matth@netsight.co.uk>
2012-08-02 16:02:25 +0100

    Modified cmfns.py to allow it to import local roles correctly. The behaviour in Marshall tried to look for <local_role> tag in the root of the tree, whereas it is actually serialized in <security><local_role></security>. This should probabaly be put into Marshall, but I don't know if that will have any side effects. As far as I can see this functionality has never worked since it was written.

--- quintagroup/transmogrifier/__init__.py
+++ quintagroup/transmogrifier/__init__.py
@@ -1,2 +1,3 @@
 # this is a package
+import namespaces
 
--- quintagroup/transmogrifier/namespaces/cmfns.py
+++ quintagroup/transmogrifier/namespaces/cmfns.py
@@ -9,6 +9,29 @@ from Products.Marshall.namespaces import cmfns as base
 class LocalRolesAttribute(base.LocalRolesAttribute):
 
     
+    def deserialize(self, instance, ns_data):
+        values = ns_data.get(self.field_id)
+        if not values:
+            return
+        for user_id, role in values:
+            instance.manage_addLocalRoles(user_id, [role])
+
+
+    def processXml(self, context, node):
+        nsprefix = node.tag[:node.tag.find('}')+1]
+        local_roles = node.findall(nsprefix+self.field_id)
+        
+        if len(local_roles) == 0:
+            return
+
+        data = context.getDataFor(self.namespace.xmlns)
+        values = data.setdefault(self.field_id, [])
+        
+        for lrole in local_roles:
+            values.append((lrole.get('user_id'), lrole.get('role')))
+        
+        return True
+
     def get(self, instance):
         """ overide local roles reader due to rare usecase of non-unicode strings in migrated Plone instances."""
         lr = getattr( instance, '__ac_local_roles__', {})
@@ -23,5 +46,5 @@ class CMF(base.CMF):
     attributes = (
         base.TypeAttribute('type'),
         base.WorkflowAttribute('workflow_history'),
-        LocalRolesAttribute('local_role')
+        LocalRolesAttribute('security','local_role'),
         )

5a20a50bad4a10da69754cf959dbdb56ad9537ca
Tom Gross <itconsense@gmail.com>
2013-07-12 21:53:20 +0200

    use correct location of INameChooser interface

--- quintagroup/transmogrifier/portlets.py
+++ quintagroup/transmogrifier/portlets.py
@@ -5,7 +5,7 @@ from zope.interface import classProvides, implements, providedBy
 from zope.component import getUtilitiesFor, queryMultiAdapter, getUtility, \
     getMultiAdapter, adapts
 from zope.component.interfaces import IFactory
-from zope.app.container.interfaces import INameChooser
+from zope.container.interfaces import INameChooser
 from zope.schema._bootstrapinterfaces import ConstraintNotSatisfied
 from zope.schema.interfaces import ICollection
 
